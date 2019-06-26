43adbdcd0ee00813effef6f2ed233126aa9d749e
Ashwin Shenoy <aswinshenoy65@gmail.com>
2019-06-26 14:25:37 +0530

    Create a sorted linkedlist using insertNode()

--- LinkedList/Main.java
+++ LinkedList/Main.java
@@ -19,12 +19,26 @@ public class Main {
         // inserting at a position
         l.insert(20,3);
         l.insert(5,1);
-
-        // Show the linked list
+        
+         // Show the linked list
         l.print();
         
-        // Find mid element of the list
-        System.out.println(l.findMid());
+        // Showing middle of the linked list
+		System.out.println(l.findMid());
+		
+		
+		//Initialize a new list
+		LinkedList s = new LinkedList();
+		
+		// Inserting elements in random order using Insert sort
+		s.insertSort(25);
+		s.insertSort(15);
+		s.insertSort(10);
+		s.insertSort(20);
+		
+		// Printing list expecting sorted list
+		s.print();
+
     }
 
 }
